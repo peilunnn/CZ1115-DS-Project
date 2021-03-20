cadef95a0b358182829ef9ad776a240fb36a0fa0
xlatios1 <woonyijun10@gmail.com>
2021-03-21 04:27:40 +0800

    Update Yijun.py

--- Yijun.py
+++ Yijun.py
@@ -2,35 +2,42 @@ import numpy as np
 import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
 from Peilun import clean_dataset
 df = clean_dataset()
 #d = df but no object type
-d = df.loc[:, df.dtypes != np.object]
+df_filtered = df.loc[:, df.dtypes != np.object]
 #Although this is a numerical data, this value everything is 0/NaN, thus unable to use
-d = d.drop(['declared_monthly_revenue'], axis=1) 
+print("shape:",df.shape)
+print("----")
+df_filtered = df.loc[:, df.dtypes != np.object]
+df_filtered = df_filtered.drop(['declared_monthly_revenue'], axis=1) #this value everything is 0/NaN
 
-def Cor():
-    print(d.corr())
-    #Using Pearson Correlation
+#Using Pearson Correlation
+def Cor_df():
+    print(df_filtered.corr())
     plt.figure(figsize=(12,10))
-    cor = d.corr()
+    cor = df_filtered.corr()
     sb.heatmap(cor, annot=True, cmap=plt.cm.Reds)
     plt.show()
+    return
 
 def Display_filtered():
-    d = pd.DataFrame(d, columns = d.columns)
-    corr_pairs = d.corr().unstack()
+    #d = pd.DataFrame(d, columns = d.columns)
+    corr_pairs = df_filtered.corr().unstack()
     sorted_pairs = corr_pairs.sort_values(kind="quicksort")
     print("---corr values that are more than 0.3 scores---\n")
     adjusted = sorted_pairs[:len(sorted_pairs)-len([x for x in sorted_pairs if x==1])]
     head = adjusted[:len([x for x in adjusted if x<-0.3])]
     tail = adjusted[len(adjusted)-len([x for x in adjusted if x>0.3]):]
     result = pd.concat([head,tail])
-    result.apply(pd.Series)
+    print(result.apply(pd.Series))
+    return
 
 def Target_corr():
-    cor = d.corr()
-    print("---Target correlation---")
+    cor = df_filtered.corr()
+    print("---Targeted correlation---")
     #Correlation with output variable
     cor_target = abs(cor["price"])
 
@@ -38,25 +45,26 @@ def Target_corr():
     relevant_features = cor_target[cor_target>0.3]
     print(relevant_features,"\n")
 
-    print("---To justify that the predictors are not very correlated and able to be used---")
-    #Will need to manual change the predicators
-    print(df[["payment_value","freight_value"]].corr())
-    print(df[["product_weight_g","payment_value"]].corr())
-
+    # print("---To justify that the predictors are not very correlated and able to be used---")
+    # #Will need to manual change the predicators
+    # print(df[["payment_value","freight_value"]].corr())
+    # print(df[["product_weight_g","payment_value"]].corr())
+    return
 
 def Model_based_fs():
-    from sklearn.tree import DecisionTreeClassifier
-    kepler_X = d.iloc[:, 1:]
-    kepler_y = d.iloc[:, 0]
+    kepler_X = df_filtered.iloc[:, 1:]
+    kepler_y = df_filtered.iloc[:, 0]
     clf = DecisionTreeClassifier()
     clf.fit(kepler_X, kepler_y)
     print("---Model-based feature selection (SelectFromModel)---")
     pd.Series(clf.feature_importances_, index=d.columns[1:]).plot.bar(color='steelblue', figsize=(12, 6))
+    plt.show()
+    return
 
 def Principal_comp():
     print("---Principal components---")
-    from sklearn.preprocessing import StandardScaler
-    cor_mat2 = np.corrcoef(d.T)
+    print("Principal components are new variables that are constructed as linear combinations or mixtures of the initial variables. These combinations are done in such a way that the new variables (i.e., principal components) are uncorrelated and most of the information within the initial variables is squeezed or compressed into the first components. So, the idea is 10-dimensional data gives you 18 principal components, but PCA tries to put maximum possible information in the first component, then maximum remaining information in the second and so on, until having something like shown in the scree plot below.")
+    cor_mat2 = np.corrcoef(df_filtered.T)
     eig_vals, eig_vecs = np.linalg.eig(cor_mat2)
 
     tot = sum(eig_vals)
@@ -72,15 +80,11 @@ def Principal_comp():
         plt.ylabel('Explained variance ratio')
         plt.xlabel('Principal components')
         plt.legend(loc='best')
-        plt.tight_layout()
-        
-    print("Principal components are new variables that are constructed as linear combinations or mixtures of the initial variables. These combinations are done in such a way that the new variables (i.e., principal components) are uncorrelated and most of the information within the initial variables is squeezed or compressed into the first components. So, the idea is 10-dimensional data gives you 18 principal components, but PCA tries to put maximum possible information in the first component, then maximum remaining information in the second and so on, until having something like shown in the scree plot below.")
+        plt.tight_layout() 
+    return
 
-if __name__ == "__main__":
-    main()
-
-
-
+print("From these we can infer that [Price] is the best predictor, followed by [payment_value] and [product_weight_g]")
+print("[Price] is the best predictor followed by [payment_value] as it has the highest correlation in regards to the dataset,and [product_weight_g] is chosen because it has the highest appearance in the top 10 correlation data.")
 
 # sources:                         
 # https://likegeeks.com/python-correlation-matrix/  
